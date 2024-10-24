5c0d720fb8ec0946df437e8c4e7d0aa626897a37
Milan G <dev@milangress.de>
2024-10-24 05:53:44 +0200

    Refactor pad.js to add localForage script and improve button behavior

--- src/static/skins/teapad/pad.js
+++ src/static/skins/teapad/pad.js
@@ -1,6 +1,18 @@
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
+window.customStart = () => {
+  $('#pad_title').show();
+  $('.buttonicon').on('mousedown', function () { $(this).parent().addClass('pressed'); });
+  $('.buttonicon').on('mouseup', function () { $(this).parent().removeClass('pressed'); });
+
+  getLocalForge();
+};
+
+
+
+
+function getLocalForge() {
   const script = document.createElement("script");
   script.src = "https://cdn.rawgit.com/mozilla/localForage/master/dist/localforage.js";
   document.head.appendChild(script);
-});
+};
