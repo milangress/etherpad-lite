da070d216de7d612e02bdd173f665aca2a554679
Milan G <dev@milangress.de>
2024-10-24 05:50:19 +0200

    Add localForage script to pad.js

---
+++ src/static/skins/teapad/pad.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.rawgit.com/mozilla/localForage/master/dist/localforage.js";
+  document.head.appendChild(script);
+});
